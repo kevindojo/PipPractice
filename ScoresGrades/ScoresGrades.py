36821bffcab9c335919b28da20d92b8c6a100650
KevinPham <kevinpham15@gmail.com>
2017-10-03 16:47:32 -0700

    undid some # on rest of function

---
+++ ScoresGrades/ScoresGrades.py
@@ -0,0 +1,8 @@
+# Score: 90 - 100; Grade - A
+# Score: 80 - 89; Grade - B
+# Score: 70 - 79; Grade - C
+# Score: 60 - 69; Grade - D
+
+def Scores_Grades(points):
+    for i in points:
+        if a in range(90, 101):
